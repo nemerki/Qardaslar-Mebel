b89126e26da02ab89c36d788960d5afb424b78c7
nemerki <mmehemmedm@gmail.com>
2018-04-02 12:53:50 +0400

    v1.0.0

---
+++ src/js/main/owlcarusel.js
@@ -0,0 +1,39 @@
+$(function(){
+	$(".partners__carousel").owlCarousel({
+		loop:true,
+		margin:10,
+		
+		autoplay:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	});
+
+
+	$(".header-carousel").owlCarousel({
+		loop:true,
+		
+		nav:true,
+		navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		autoplay:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+});
