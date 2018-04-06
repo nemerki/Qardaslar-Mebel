5a4537b738a25e003cedff05b0722ea71dcc4aa0
nemerki <mmehemmedm@gmail.com>
2018-04-06 18:11:06 +0400

    shop-grid bitti index ve single-product baslandi

--- src/js/main/owlcarusel.js
+++ src/js/main/owlcarusel.js
@@ -36,4 +36,24 @@ $(function(){
 			}
 		}
 	});
+
+	$(".featured__carousel").owlCarousel({
+		loop:true,
+		nav:true,
+		navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		dots :false,
+		autoplay:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:4
+			}
+		}
+	});
+
 });
