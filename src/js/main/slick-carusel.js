3dffbb1e310520dcf6fa76267be781347e3d9f14
nemerki <mmehemmedm@gmail.com>
2018-04-09 01:25:04 +0400

    deal-week bitti

--- src/js/main/slick-carusel.js
+++ src/js/main/slick-carusel.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
-	$(document).ready(function(){
-		$('.carousel').slick({
-			slidesToShow: 3,
-			dots:true,
-			centerMode: true,
-		});
+	$('.center').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		dots:true,
+		//autoplay: true,
+		//arrows: true,
+		nextArrow:'<button class="slick-next" type="button"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+		prevArrow: '<button class="slick-prev" type="button"> <i class="fa fa-angle-left" aria-hidden="true"></i> </button> ',
+
+		responsive: [
+		{
+			breakpoint: 991,
+			settings: {
+				//arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				//arrows: false,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1
+			}
+		}
+		]
 	});
 });
+
+
